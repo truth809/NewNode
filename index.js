05215b01b7867afeddec21eb2997ce6621a7b869
truth809 <truth809@naver.com>
2021-02-07 17:29:40 +0900

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 유저 가져오기
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://truth809:123123qq@cluster0.oxuxg.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -16,3 +16,27 @@ mongoose.connect('mongodb+srv://truth809:123123qq@cluster0.oxuxg.mongodb.net/<db
 	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB connect success...'))
 .catch(err => console.log('errrrrrrrrrrr',err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+    // 회원가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    // 몽고DB 메소드
+    // req.body의 정보들이 user모델에 저장, 콜백펑션
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
