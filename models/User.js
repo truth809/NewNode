6866cd39b80355d919b320704664c0592b2c9ed2
truth809 <truth809@naver.com>
2021-02-07 21:32:05 +0900

    11.start

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
     name: {
         type:String,
@@ -29,6 +32,25 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    // 패스워드가 변환 될때만
+    if(user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+    }
+})
+
+
 //스키마를 모델로 감싸기
 const User = mongoose.model('User', userSchema)
 //다른 곳에서 쓸수잇게
