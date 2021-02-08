aaf8537bdf68d4b7666436049aefcaeb7ba1da99
truth809 <truth809@naver.com>
2021-02-08 15:35:44 +0900

    13.start

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('새해 복 많이 받으세요!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     // 회원가입 할때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -45,7 +45,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user) {
