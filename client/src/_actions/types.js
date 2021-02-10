98eea425c52fb26ca84f614801caa5ca995fb58b
truth809 <truth809@naver.com>
2021-02-10 23:50:17 +0900

    32.start

--- client/src/_actions/types.js
+++ client/src/_actions/types.js
@@ -1,1 +1,2 @@
 export const LOGIN_USER = "login_user";
+export const REGISER_USER = "register_user";
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,9 +1,12 @@
-import { LOGIN_USER } from '../_actions/types';
+import { LOGIN_USER, REGISER_USER } from '../_actions/types';
 
 export default function(state = {}, action) {
     switch(action.type) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
+            break;
+        case REGISER_USER:
+            return { ...state, register: action.payload }
             break;
 
         default:
